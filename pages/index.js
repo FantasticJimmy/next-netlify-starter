3b299ab4c7dc762354f0c0efc032aa37ef3c6c3b
fantasticjimmy <dboyjimmy@gmail.com>
2022-09-06 09:26:24 -0400

    feat: added server functionality

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import { capitalizeFirstLetter } from "../lib/ultis";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Home() {
+  const { data, error } = useSWR("/api/peoples/1", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
+  const name = capitalizeFirstLetter(data.name);
   return (
     <div className="container">
       <Head>
@@ -11,7 +21,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Bonni I love you" />
+        <Header title={`${name} I love you`} />
         <p className="description">- Jimmy</p>
       </main>
 
