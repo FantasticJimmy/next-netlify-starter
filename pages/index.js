f11af8be849b760c54cc1a519ec865fac765d6aa
fantasticjimmy <dboyjimmy@gmail.com>
2023-01-11 22:12:16 -0500

    add countdown

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import { capitalizeFirstLetter } from "../lib/ultis";
 import useSWR from "swr";
+import Countdown, {
+  zeroPad,
+  calcTimeDelta,
+  formatTimeDelta,
+} from "react-countdown";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -12,6 +17,7 @@ export default function Home() {
   if (error) return <div>Failed to load</div>;
   if (!data) return <div>Loading...</div>;
 
+  const feb25 = new Date("2023-02-25");
   const name = capitalizeFirstLetter(data.name);
   return (
     <div className="container">
@@ -23,9 +29,23 @@ export default function Home() {
       <main>
         <Header title={`${name} I love you`} />
         <p className="description">- Jimmy</p>
+        <Countdown date={feb25} renderer={renderer}>
+          <Completionist />
+        </Countdown>
       </main>
 
       <Footer />
     </div>
   );
 }
+
+const renderer = ({ days, hours, minutes, seconds }) => (
+  <div style={{ textAlign: "center" }}>
+    {`${zeroPad(days)} days ${zeroPad(hours)} hours ${zeroPad(minutes)} minutes
+    ${zeroPad(seconds)} seconds`}
+    <br />
+    LEFT TO GO TO JAPAN!
+  </div>
+);
+
+const Completionist = () => <div>JAPAN!</div>;
